e6cf7e2d194d0299ade0de7ad581f1e4a0898d3a
renata.sds09@gmail.com <renata09>
2017-09-12 15:58:57 -0300

    Start game
    Signed-off-by: Renata Soares dos Santos <renata.sds09@gmail.com>
    Signed-off-by: JoÃo Vitor Araujo <joao18araujo@gmail.com>

---
+++ Assets/CameraMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)){
+			gameObject.transform.Translate(Vector3.forward);
+	 	}
+
+		if(Input.GetKey(KeyCode.DownArrow)){
+			gameObject.transform.Translate(Vector3.back);
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			gameObject.transform.Rotate(new Vector3(0, -speed * Time.deltaTime, 0));
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			gameObject.transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+		}
+	}
+}
