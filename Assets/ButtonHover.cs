e96bf54ac5afe0e20bed940e6a723cd93812e718
Sabryna de Sousa <sabryna.sousa1323@gmail.com>
2017-09-30 18:39:21 -0300

    Create menu and cursor
    Signed-off-by: Sabryna de Sousa <sabryna.sousa1323@gmail.com>
    Signed-off-by: Renata Soares <renatasds.09@gmail.com>

---
+++ Assets/ButtonHover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHover : MonoBehaviour {
+	public int timeRemaining = 5;
+	void countDown(){
+		timeRemaining--;
+		print(timeRemaining);
+		if(timeRemaining <= 0){
+			goToScene();
+			CancelInvoke("countDown");
+			timeRemaining = 5;
+		}
+	}
+	
+	void goToScene(){
+		print("Selecionou");
+	}
+	
+	public void MouseOver(){
+		print("entrouu");
+		InvokeRepeating("countDown", 1,1);
+	}
+	
+	public void MouseOut(){
+		CancelInvoke("countDown");
+		timeRemaining = 5;
+	}
+}
