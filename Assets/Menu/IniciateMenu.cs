2ca2acf2440c36aacad0b5d8c124ff99a47cd20e
renata.sds09@gmail.com <renata09>
2017-10-18 17:47:22 -0200

    Updating functions on iniciate menu

--- Assets/Menu/IniciateMenu.cs
+++ Assets/Menu/IniciateMenu.cs
@@ -2,33 +2,11 @@
 using System.Collections;
 using UnityEngine.UI;
 
+public class IniciateMenu : MenuButton {
 
-public class IniciateMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		GameObject.Find("Arrow2").GetComponent<Button>().enabled = false;
-		GameObject.Find("Arrow2").transform.localScale = new Vector3(0,0,0);
-
-		GameObject.Find("Play").GetComponent<Button>().enabled = false;
-		GameObject.Find("Play").transform.localScale = new Vector3(0,0,0);
-
-		GameObject.Find ("Girl").GetComponent<Button> ().enabled = false;
-		GameObject.Find("Girl").transform.localScale = new Vector3(0,0,0);
+	void Start (){
+		string[] hiddenButtons = new string[] {"Arrow2", "Play", "Girl", "Boy", "Running", "Free"};
 		
-		GameObject.Find("Boy").GetComponent<Button>().enabled = false;
-		GameObject.Find("Boy").transform.localScale = new Vector3(0,0,0);
-
-		GameObject.Find ("Running").GetComponent<Button> ().enabled = false;
-		GameObject.Find("Running").transform.localScale = new Vector3(0,0,0);
-		
-		GameObject.Find("Free").GetComponent<Button>().enabled = false;
-		GameObject.Find("Free").transform.localScale = new Vector3(0,0,0);
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		disableButton(hiddenButtons);
 	}
 }
