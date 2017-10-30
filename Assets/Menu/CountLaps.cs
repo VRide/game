ddcdab6144c96999d6b89030129c432598230958
Sabryna de Sousa <sabryna.sousa1323@gmail.com>
2017-10-30 10:27:43 -0200

    Group buttons into painels
    Signed-off-by: Mateus Manuel <mateusmanuel21@gmail.com>
    Signed-off-by: Sabryna de Sousa <sabryna.sousa1323@gmail.com>

---
+++ Assets/Menu/CountLaps.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountLaps : MonoBehaviour {
+	
+	private static int timeOver = 1;
+	private int timeRemaining;
+
+	void Start(){
+		timeRemaining = timeOver;
+		PlayerInfo.laps = 1;
+	}
+
+	public void countDown(){
+		timeRemaining--;
+		
+		if(timeRemaining <= 0){
+			timeRemaining = timeOver;
+			countLaps();
+		}
+	}
+
+	public void MouseOver(){
+		InvokeRepeating("countDown", 1,1);
+	}
+	
+	public void MouseOut(){
+		CancelInvoke("countDown");
+		timeRemaining = timeOver;
+	}
+
+	private void countLaps(){
+		if(gameObject.name == "RoundMinor"){
+			if (PlayerInfo.laps > 1){
+				PlayerInfo.laps--;	
+				print("--");
+			}
+		}
+		else if (gameObject.name == "RoundPlus"){
+			PlayerInfo.laps++;
+		}
+
+		print (PlayerInfo.laps);
+
+		Button buttonOne; 
+		buttonOne = GameObject.Find ("NumberTurns").GetComponentInChildren<Button> ();
+		buttonOne.GetComponentInChildren<Text>().text = PlayerInfo.laps.ToString();
+	}
+
+}
---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo {
+
+	public enum Modes { Free, Running }
+
+	public static int mode {get; set;}
+	public static int laps {get; set;}
+}
