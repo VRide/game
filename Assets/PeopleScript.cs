c9613b1d10d7f86b122baa324ea006c115275613
Renata Soares <renata.sds09@gmail.com>
2017-11-07 16:10:08 -0200

    Woman or man logic
    Signed-off-by: JoÃo Vitor Araujo <joao18araujo@gmail.com>
    Signed-off-by: Renata Soares <renata.sds09@gmail.com>

---
+++ Assets/PeopleScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeopleScript : MonoBehaviour {
+
+	private GameObject man;
+	private GameObject woman;
+
+	// Use this for initialization
+	void Start () {
+		man = GameObject.Find ("man");
+		woman = GameObject.Find ("woman");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.H)) {
+			woman.SetActive(false);
+		}
+		if (Input.GetKey (KeyCode.M)) {
+			man.SetActive(false);
+		}
+	}
+}
